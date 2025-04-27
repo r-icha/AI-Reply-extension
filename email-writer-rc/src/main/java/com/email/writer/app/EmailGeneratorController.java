c1cfc29a95e682ad898a6a438fabbf6581a23dd1
Richa Chaudhary <66511611+r-icha@users.noreply.github.com>
2025-04-27 21:49:56 +0530

    Add files via upload

---
+++ email-writer-rc/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -0,0 +1,27 @@
+package com.email.writer.app;
+
+import lombok.AllArgsConstructor;
+//import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+//    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+//        this.emailGeneratorService = emailGeneratorService;
+//    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
+
